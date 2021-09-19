7e315779c6909773e11f573e333e13b9dd3c5911
GrandBee <90036008+GrandBee@users.noreply.github.com>
2021-09-19 13:24:02 +0300

    Add files via upload

---
+++ BooleanTesting.java
@@ -0,0 +1,13 @@
+class BooleanTesting {
+    public static void main(String[] args) {
+        boolean bool;
+        bool = false;
+        System.out.println("У мене ще нема 100 грн");
+        bool = true;
+        System.out.println("У мене вже є 100 грн");
+        if (bool)
+            System.out.println(bool);
+        bool = false;
+
+    }
+}
